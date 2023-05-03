4017c1704981b2f20c3f595c9235e88f3f761c37
Ate Pelsma <87018841+ate-pelsma@users.noreply.github.com>
2023-05-03 16:58:25 +0200

    API en boek opslaan werkt

--- src/SearchIsbn.js
+++ src/SearchIsbn.js
@@ -1,33 +1,43 @@
 import { useState, useEffect } from "react";
-import { BookInfo } from "./BookInfo";
+import { CreateBook } from "./CreateBook";
 
-export const SearchIsbn = () => {
+export const SearchIsbn = ({createBook}) => {
     
     const [bookObject, setBookObject] = useState(null)
     const [currentIsbn, setCurrentIsbn] = useState("")
+    const [alert, setAlert] = useState(false)
 
     const fetchBook = () => {
         fetch(`https://openlibrary.org/api/books?bibkeys=ISBN:${currentIsbn}&jscmd=data&format=json`)
             .then(r => r.json())
             .then(d => {
                 for(let key in d){
-                    const responseData  = d[key];
+                    let responseData  = d[key];
                     setBookObject(responseData)
+                    setAlert(false)
+                }
+                if(Object.keys(d).length === 0
+                && Object.getPrototypeOf(d) === Object.prototype){
+                    setAlert(true)
                 }
             })
     }
 
-    const element = bookObject ? <BookInfo data={bookObject} /> : <h1>No book found yet</h1>
-    
+    const rerenderBook = bookObject ? <CreateBook data={bookObject} isbn={currentIsbn} /> : <h1>No book found yet</h1>
+    const visibility = createBook ? {visibility: 'visible'} : {visibility: 'hidden'}
+    const alertElement = alert ? <div>Alert message</div> : <div></div>
+
     // useEffect(() => {
-        
-    // }, [bookObject])
+    //     console.log("rerender")
+    //     console.log(alert)
+    // }, [alert])
 
     return (
-        <div>
+        <div style={visibility}>
             <input value={currentIsbn} onChange={(e) => setCurrentIsbn(e.target.value)} type="text" placeholder="type isbn here"></input>
             <button type="button" onClick={fetchBook}></button>
-            {element}
+            {alertElement}
+            <CreateBook data={bookObject} isbn={currentIsbn} alert={alert} />
         </div>
     )
 }
