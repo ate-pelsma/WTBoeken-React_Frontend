b30863b8bd1a12459cf48e2ae519d4df7cf00ed3
GeoffryMuijtstege <67443113+GeoffryMuijtstege@users.noreply.github.com>
2023-05-11 14:51:01 +0200

    Navbar reactive gemaakt

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import {Link} from "react-router-dom";
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import jwtDecode from 'jwt-decode';
 import { useLocalState } from './utils/setLocalStorage';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import NavDropdown from 'react-bootstrap/NavDropdown';
 
 export default function NavBar()
 {
   const [jwt, setJwt] = useLocalState("", "jwt");
   const [roles, setRoles] = useState(getRolesFromJWT());
+  const [navBarLinks, setNavBarLinks] = useState("");
 
   function getRolesFromJWT(){
     if(jwt){
@@ -17,84 +22,61 @@ export default function NavBar()
     return [];
   }
 
-let header;
-switch (true) {
-  case roles.includes("ROLE_ADMIN"):
-    header = (
-      <header>
-        <nav className="navbar">
-          <img
-            src={require("./images/WorkingTalentLogo.png")}
-            alt="Workingtalent Logo"
-            className="logo"
-          />
-          <ul className="nav-links">
-            <li>
-              <Link to="/">Catalogus</Link>
-            </li>
-            <li>
-              <Link to="/MyReservations">Mijn Reserveringen</Link>
-            </li>
-            <li>
-              <Link to="/MyAccount">Mijn Account</Link>
-            </li>
-            <li>
-              <Link to="/books">Boekenoverzicht</Link>
-            </li>
-            <li>
-              <Link to="/users">Gebruikers overzicht</Link>
-            </li>
-            <li>
-              <Link to="/ReservationsSummary">Reserveringen overzicht</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-    break;
-  case roles.includes("ROLE_USER"):
-    header = (
-      <header>
-        <nav className="navbar">
-        <img
-            src={require("./images/WorkingTalentLogo.png")}
-            alt="Workingtalent Logo"
-            className="logo"
-          />
-          <ul className="nav-links">
-            <li>
-              <Link to="/">Catalogus</Link>
-            </li>
-            <li>
-              <Link to="/MyReservations">Mijn Reserveringen</Link>
-            </li>
-            <li>
-              <Link to="/MyAccount">Mijn Account</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-    break;
-  default:
-    header = (
-      <header>
-        <nav className="navbar">
-        <img
-            src={require("./images/WorkingTalentLogo.png")}
-            alt="Workingtalent Logo"
-            className="logo"
-          />
-          <ul className="nav-links">
-            <li>
-              <Link to="/login">Log in</Link>
-            </li>
-          </ul>
-        </nav>
-      </header>
-    );
-    break;
-}
+  function setNavBar(){
+    console.log(roles);
+    let links;
+    if(roles[0] === ("ROLE_ADMIN")){
+      links = (
+          <>
+            <Nav.Link href="/" className="navLinks">Catalogus</Nav.Link>
+            <Nav.Link href="/MyReservations" className="navLinks">Mijn Reserveringen</Nav.Link>
+            <Nav.Link href="/MyAccount" className="navLinks">Mijn Account</Nav.Link>
+            <Nav.Link href="/books" className="navLinks">Boekenoverzicht</Nav.Link>
+            <Nav.Link href="/users" className="navLinks">Gebruikers overzicht</Nav.Link>
+            <Nav.Link href="/ReservationsSummary" className="navLinks">Reserveringen overzicht</Nav.Link>
+          
+          </>
+      );
+    }
+    else if(roles[0] === ("ROLE_USER")){
+      links = (
+        <>
+            <Nav.Link href="/" className="navLinks">Catalogus</Nav.Link>
+            <Nav.Link href="/MyReservations" className="navLinks">Mijn Reserveringen</Nav.Link>
+            <Nav.Link href="/MyAccount" className="navLinks">Mijn Account</Nav.Link>
+        </>
+      );
+    }
+    else{
+      links = (
+        <Nav.Link href="/login" className="navLinks">Log in</Nav.Link>
+      );
+    }
+    setNavBarLinks(links)
+  }
 
-return header;
+  useEffect(() => {
+    setNavBar()
+}, [])
+
+  return (
+    <Navbar sticky="top" bg="white" expand="lg">
+      <Container margin="0">
+        <Navbar.Brand>
+          <img src=
+                    {require("./images/WorkingTalentLogo.png")}
+                    alt="Workingtalent Logo"
+                    className="logo"
+                    />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="me-auto">
+            {navBarLinks}
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
+
 }
